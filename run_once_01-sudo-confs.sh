10dad25d4ad657b1ae3c4f0c1dee20da9d83a2ae
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-10-06 11:44:44 +0000

    tee

--- run_once_01-sudo-confs.sh
+++ run_once_01-sudo-confs.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 sudo mkdir -p /etc/systemd/logind.conf.d/
-sudo echo 'HandlePowerKey=ignore' > /etc/systemd/logind.conf.d/40-powerbutton.conf
+echo 'HandlePowerKey=ignore' | sudo tee /etc/systemd/logind.conf.d/40-powerbutton.conf
 
-sudo echo '[zram0]
-zram-fraction = 0.5' > /etc/systemd/zram-generator.conf
+echo '[zram0]
+zram-fraction = 0.5' | sudo tee /etc/systemd/zram-generator.conf
 
 
 sudo mkdir -p /etc/NetworkManager/dispatcher.d
-sudo echo '#!/bin/sh
+
+echo '#!/bin/sh
 case "$2" in 
     up)
         timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
     ;;
-esac' > /etc/NetworkManager/dispatcher.d/09-timezone
+esac' | sudo tee /etc/NetworkManager/dispatcher.d/09-timezone
 
 sudo timedatectl set-ntp true
-
 sudo systemctl enable reflector
