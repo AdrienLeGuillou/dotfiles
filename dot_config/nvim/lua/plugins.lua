a3b44b4526dfc5faf8a7c69dbbf176c3aa58c89e
Adrien Le Guillou <git@aleguillou.org>
2022-03-31 16:15:24 +0200

    radian

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -362,29 +362,22 @@ use {
     'hkupty/iron.nvim',
     config = function()
       local iron = require('iron')
+
+      iron.core.add_repl_definitions {
+        r = {
+          radian = {
+            command = {"radian"}
+          }
+        },
+      }
+
       iron.core.set_config {
         repl_open_cmd = 'vertical 85 split',
         highlight_last = false,
+        preferred = {
+          r = "radian",
+        },
       }
-      -- iron.core.add_repl_definitions {
-      --   python = {
-      --     mycustom = {
-      --       command = {"mycmd"}
-      --     }
-      --   },
-      --   clojure = {
-      --     lein_connect = {
-      --       command = {"lein", "repl", ":connect"}
-      --     }
-      --   }
-      -- }
-      --
-      -- iron.core.set_config {
-      --   preferred = {
-      --     python = "ipython",
-      --     clojure = "lein"
-      --   }
-      -- }
     end,
   })
 
