8f8f09f5cc3d0bef098d8bb6765261e445e96e09
Adrien Le Guillou <git@aleguillou.org>
2024-01-16 13:10:04 +0100

    snippets, finally

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -32,14 +32,35 @@ return {
 		end,
 	},
   {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function ()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      -- local ls = require("luasnip")
+      -- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+      -- vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+      -- vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+      --
+      -- vim.keymap.set({"i", "s"}, "<C-E>", function()
+      --   if ls.choice_active() then
+      --     ls.change_choice(1)
+      --   end
+      -- end, {silent = true})
+    end
+  },
+  {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lua',
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       cmp.setup({
         mapping = {
@@ -50,25 +71,32 @@ return {
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
+
+          -- Ctrl-l: start completion, validate it or go to next snippet elt
+          ['<C-l>'] =  cmp.mapping(function(fallback)
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            -- elseif cmp.visible() then
+            --   cmp.mapping.confirm({ select = true })
             else
               fallback()
             end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end,
+          end, { "i", "s" }),
+          -- Ctrl-h: go to previous snippet elt
+          ['<C-h>'] = cmp.mapping(function(fallback)
+              if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+
         },
         preselect = cmp.PreselectMode.None,
         sources = {
           { name = 'nvim_lsp' },
           { name = 'buffer' },
+          { name = 'luasnip' },
           {
             name = 'path',
             option = {
@@ -79,7 +107,12 @@ return {
             },
           },
           { name = 'nvim-lua' },
-        }
+        },
+        snippet = {
+          expand = function(args)
+            require'luasnip'.lsp_expand(args.body)
+          end
+        },
       })
     end,
   },
