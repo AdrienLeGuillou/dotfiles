be36aef4a8a50fc114e5cf7ac5de45d74dfc7d62
Adrien Le Guillou <git@aleguillou.org>
2024-03-26 17:55:05 -0400

    add specter with config

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -32,6 +32,15 @@ return {
 		end,
 	},
   {
+    'nvim-pack/nvim-spectre',
+    config = function()
+      vim.keymap.set('n', '<leader>sg', '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
+      vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', { desc = "Search current word" })
+      vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
+      vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Search on current file" })
+    end
+  },
+  {
     'glacambre/firenvim',
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
