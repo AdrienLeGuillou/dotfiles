fef3e4ef88f4ad92aeb60651769b5751e6dc518e
Adrien Le Guillou <git@aleguillou.org>
2024-06-11 22:08:16 +0200

    only 4 space for c code

--- dot_config/nvim/after/ftplugin/c.lua
+++ dot_config/nvim/after/ftplugin/c.lua
@@ -1,4 +1,4 @@
-local indent = 8
+local indent = 4
 vim.o.shiftwidth = indent             -- Size of an indent
 vim.o.tabstop = indent                -- Number of spaces tabs count for
 vim.o.softtabstop = indent
