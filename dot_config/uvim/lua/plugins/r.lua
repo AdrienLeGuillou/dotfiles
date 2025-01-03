19dd479d7dd61cb5cfe2d67461c6f7831b1dfed7
Adrien Le Guillou <git@aleguillou.org>
2025-01-03 10:46:00 +0100

    <-

--- dot_config/uvim/lua/plugins/r.lua
+++ dot_config/uvim/lua/plugins/r.lua
@@ -12,51 +12,56 @@ return {
       user_maps_only = true,
       open_pdf = "no",
       pdfviewer = "",
+      pipe_keymap = "",
+      assignment_keymap = "",
 
-        hook = {
-          on_filetype = function ()
-            -- This function will be called at the FileType event
-            -- of files supported by R.nvim. This is an
-            -- opportunity to create mappings local to buffers.
-            --
-              -- R management
-              vim.keymap.set("n", "<LocalLeader>xx", "<Plug>RStart")
-              vim.keymap.set("n", "<LocalLeader>xq", "<Plug>RClose")
-              vim.keymap.set("n", "<LocalLeader>xk", "<Cmd>RKill<CR>")
-              vim.keymap.set("n", "<LocalLeader>xi", "<Cmd>RStop<CR>")
-              -- RObjectBrowser
-              vim.keymap.set("n", "<LocalLeader>xo", "<Plug>ROBToggle")
-              -- Send Cmds
-              vim.keymap.set("n", "<LocalLeader><LocalLeader>", "<Plug>RSendLine")
-              vim.keymap.set("v", "<LocalLeader><LocalLeader>", "<Plug>RSendSelection")
-              -- Inspect Cmds
-              vim.keymap.set("n", "<LocalLeader>ii", "<Plug>RObjectPr")
-              vim.keymap.set("n", "<LocalLeader>is", "<Plug>RSummary")
-              vim.keymap.set("n", "<LocalLeader>it", "<Plug>RObjectStr")
-              vim.keymap.set("n", "<LocalLeader>in", "<Plug>RObjectNames")
-              vim.keymap.set("n", "<LocalLeader>ih", "<Plug>RHelp")
-              vim.keymap.set("n", "<LocalLeader>il", "<Plug>RListSpace")
+      hook = {
+        on_filetype = function()
+          -- This function will be called at the FileType event
+          -- of files supported by R.nvim. This is an
+          -- opportunity to create mappings local to buffers.
+          --
+          -- R management
+          vim.keymap.set("n", "<LocalLeader>xx", "<Plug>RStart")
+          vim.keymap.set("n", "<LocalLeader>xq", "<Plug>RClose")
+          vim.keymap.set("n", "<LocalLeader>xk", "<Cmd>RKill<CR>")
+          vim.keymap.set("n", "<LocalLeader>xi", "<Cmd>RStop<CR>")
+          -- RObjectBrowser
+          vim.keymap.set("n", "<LocalLeader>xo", "<Plug>ROBToggle")
+          -- Send Cmds
+          vim.keymap.set("n", "<LocalLeader><LocalLeader>", "<Plug>RSendLine")
+          vim.keymap.set("v", "<LocalLeader><LocalLeader>", "<Plug>RSendSelection")
+          -- Inspect Cmds
+          vim.keymap.set("n", "<LocalLeader>ii", "<Plug>RObjectPr")
+          vim.keymap.set("n", "<LocalLeader>is", "<Plug>RSummary")
+          vim.keymap.set("n", "<LocalLeader>it", "<Plug>RObjectStr")
+          vim.keymap.set("n", "<LocalLeader>in", "<Plug>RObjectNames")
+          vim.keymap.set("n", "<LocalLeader>ih", "<Plug>RHelp")
+          vim.keymap.set("n", "<LocalLeader>il", "<Plug>RListSpace")
 
-              -- Targets
-              vim.keymap.set("n", "<LocalLeader>mm", "<Cmd>lua require('r.send').cmd([[targets::tar_make()]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>mc", "<Cmd>lua require('r.send').cmd([[targets::tar_destroy()]])<CR>")
+          -- Targets
+          vim.keymap.set("n", "<LocalLeader>mm", "<Cmd>lua require('r.send').cmd([[targets::tar_make()]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>mc", "<Cmd>lua require('r.send').cmd([[targets::tar_destroy()]])<CR>")
 
-              -- Devtools
-              vim.keymap.set("n", "<LocalLeader>pd", "<Cmd>lua require('r.send').cmd([[devtools::document()]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>pv", "<Cmd>lua require('r.send').cmd([[devtools::build_vignettes()]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>pT", "<Cmd>lua require('r.send').cmd([[devtools::check()]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>pt", "<Cmd>lua require('r.send').cmd([[devtools::test()]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>pl", "<Cmd>lua require('r.send').cmd([[devtools::load_all()]])<CR>")
+          -- Devtools
+          vim.keymap.set("n", "<LocalLeader>pd", "<Cmd>lua require('r.send').cmd([[devtools::document()]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>pv", "<Cmd>lua require('r.send').cmd([[devtools::build_vignettes()]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>pT", "<Cmd>lua require('r.send').cmd([[devtools::check()]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>pt", "<Cmd>lua require('r.send').cmd([[devtools::test()]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>pl", "<Cmd>lua require('r.send').cmd([[devtools::load_all()]])<CR>")
 
-              -- Renv
-              vim.keymap.set("n", "<LocalLeader>ri", "<Cmd>lua require('r.send').cmd([[renv::init(bare = TRUE)]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>re", "<Cmd>lua require('r.send').cmd([[renv::install(c('languageserver', 'devtools', 'pak'))]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>ru", "<Cmd>lua require('r.send').cmd([[renv::update(prompt=F)]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>rs", "<Cmd>lua require('r.send').cmd([[renv::snapshot(prompt=F)]])<CR>")
-              vim.keymap.set("n", "<LocalLeader>rr", "<Cmd>lua require('r.send').cmd([[renv::restore(prompt=F)]])<CR>")
+          -- Renv
+          vim.keymap.set("n", "<LocalLeader>ri", "<Cmd>lua require('r.send').cmd([[renv::init(bare = TRUE)]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>re",
+            "<Cmd>lua require('r.send').cmd([[renv::install(c('languageserver', 'devtools', 'pak'))]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>ru", "<Cmd>lua require('r.send').cmd([[renv::update(prompt=F)]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>rs", "<Cmd>lua require('r.send').cmd([[renv::snapshot(prompt=F)]])<CR>")
+          vim.keymap.set("n", "<LocalLeader>rr", "<Cmd>lua require('r.send').cmd([[renv::restore(prompt=F)]])<CR>")
 
-          end
-        },
+          -- Editor
+          vim.keymap.set("i", "<A-->", "<-")
+        end
+      },
     },
     init = function()
       vim.g.rout_follow_colorscheme = true
