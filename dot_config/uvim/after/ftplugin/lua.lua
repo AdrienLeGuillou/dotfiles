f02e76b0d0de3115a2ed8d23e54e3d95ea7b19cf
Adrien Le Guillou <git@aleguillou.org>
2025-01-24 13:43:42 +0100

    asm

--- dot_config/uvim/after/ftplugin/lua.lua
+++ dot_config/uvim/after/ftplugin/lua.lua
@@ -2,4 +2,3 @@ local set = vim.opt_local
 set.shiftwidth = 2
 set.tabstop = 2
 set.softtabstop = 2
-set.expandtab = true
--- dot_config/uvim/after/ftplugin/r.lua
+++ dot_config/uvim/after/ftplugin/r.lua
@@ -2,7 +2,6 @@ local set = vim.opt_local
 set.shiftwidth = 2
 set.tabstop = 2
 set.softtabstop = 2
-set.expandtab = true
 
 -- better indent with R.nvim
 vim.cmd([[ let r_indent_align_args = 0 ]])
