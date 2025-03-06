c8b16c07ccb346651cdd3f28438e24debb548e34
Adrien Le Guillou <git@aleguillou.org>
2025-03-06 10:46:13 +0100

    fix telescope

--- dot_config/uvim/lua/plugins/telescope.lua
+++ dot_config/uvim/lua/plugins/telescope.lua
@@ -41,9 +41,9 @@ return {
       end)
     end,
     keys = {
-      { "<leader>sh", require('telescope.builtin').help_tags,  desc = "Search Help" },
-      { "<leader>ff", require('telescope.builtin').find_files, desc = "Find Files" },
-      { "<leader>fF", "<CMD>lua require('telescope.builtin').find_files({hidden = true, no_ignore = true})<CR>", desc = "Open file (Show All)" },
+      { "<leader>sh", "<cmd>lua require('telescope.builtin').help_tags<CR",  desc = "Search Help" },
+      { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files<CR", desc = "Find Files" },
+      { "<leader>fF", "<cmd>lua require('telescope.builtin').find_files({hidden = true, no_ignore = true})<CR>", desc = "Open file (Show All)" },
       { "<leader>se", "<cmd>Telescope command_history:<CR>", desc = "Search command history"},
       { "<leader>sf", "<cmd>Telescope live_grep<CR>", desc = "Search line in files"},
       { "<leader>sl", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Search line in buffer"},
