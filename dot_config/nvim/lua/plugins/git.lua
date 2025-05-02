402ba791ce81dcee7f09bb21a2a796370b98dea1
Adrien Le Guillou <git@aleguillou.org>
2025-05-02 11:35:08 +0200

    add diffview

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,10 +1,3 @@
 return {
-  {
-    'lewis6991/gitsigns.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim'
-    },
-    config = true,
-  },
-  'tpope/vim-fugitive',
+  "sindrets/diffview.nvim"
 }
